b2811c76fb756f29358fe771e9af8057f12c2e15
hendurhance <50846992+hendurhance@users.noreply.github.com>
2021-01-14 20:14:10 +0100

    app vue getting fixed

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,85 @@
+<template>
+  <header>
+      <nav>
+          <h1><router-link to="/">findTutor</router-link></h1>
+          <ul>
+              <li>
+                  <router-link to="/tutors">All Tutors</router-link>
+              </li>
+              <li>
+                  <router-link to="/requests">Requests</router-link>
+              </li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+header {
+  width: 100%;
+  height: 5rem;
+  background-color:#04096e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.06), 0 3px 6px rgba(0, 0, 0, 0.09);
+}
+
+header a {
+  text-decoration: none;
+  color: #fff;
+  display: inline-block;
+  padding: 0.75rem 1.5rem;
+  border: 1px solid transparent;
+}
+
+a:active,
+a:hover,
+a.router-link-active {
+  border: 1px solid #fff;
+}
+
+h1 {
+  margin: 0;
+}
+
+h1 a {
+  color: white;
+  margin: 0;
+}
+
+h1 a:hover,
+h1 a:active,
+h1 a.router-link-active {
+  border-color: transparent;
+}
+
+header nav {
+  width: 90%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+header ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li {
+  margin: 0 0.5rem;
+}
+
+</style>
--- src/pages/tutors/TutorsList.vue
+++ src/pages/tutors/TutorsList.vue
@@ -1,5 +1,10 @@
 <template>
-  
+  <section>
+      FILTER
+  </section>
+  <section>
+      LIST OF TUTORS
+  </section>
 </template>
 
 <script>
