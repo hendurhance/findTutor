0138b6e27957dabf540b7a9393e3778e5dbe205c
hendurhance <50846992+hendurhance@users.noreply.github.com>
2021-01-18 11:16:56 +0100

    tutorlist chnages

--- src/pages/tutors/TutorsList.vue
+++ src/pages/tutors/TutorsList.vue
@@ -5,24 +5,48 @@
           <button>Refresh</button>
           <router-link to="/register">Register as Tutor</router-link>
       </div>
-      <ul> 
-        <li v-for="tutor in filteredTutors" :key="tutor.id">
-            {{ tutor.firstName }}
-        </li> 
+      <ul v-if="hasCoaches"> 
+        <tutor-item 
+        v-for="tutor in filteredTutors" 
+        :key="tutor.id"
+        :id="tutor.id"
+        :first-name="tutor.firstName"
+        :last-name="tutor.lastName"
+        :rate="tutor.hourlyRate"
+        :areas="tutor.areas"
+        ></tutor-item> 
       </ul>
+      <h3 v-else>No tutors available.</h3>
   </section>
 </template>
 
 <script>
+import TutorItem from '../../components/tutors/TutorItem.vue'
+
 export default {
+    components: {
+        TutorItem
+    },
     computed: {
         filteredTutors(){
             return this.$store.getters['tutors/tutors']
+        },
+        hasCoaches(){
+            return this.$store.getters['tutors/hasTutors']
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
