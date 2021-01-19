579ae76f226279a5a2074c482745a7339139e114
hendurhance <50846992+hendurhance@users.noreply.github.com>
2021-01-19 12:53:07 +0100

    form validation working

--- src/components/tutors/TutorForm.vue
+++ src/components/tutors/TutorForm.vue
@@ -1,36 +1,80 @@
 <template>
   <form @submit.prevent="submitForm">
-      <div class="form-control">
+      <div class="form-control" :class="{invalid: !firstName.isValid}">
           <label for="firstname">Firstname</label>
-          <input type="text" id="firstname" v-model.trim="firstName">
+          <input 
+            type="text" 
+            id="firstname" 
+            v-model.trim="firstName.val" 
+            @blur="clearValidity('firstName')"
+          >
+          <p v-if="!firstName.isValid">Nerd, fill the Firstname field</p>
       </div>
-      <div class="form-control">
+      <div class="form-control" :class="{invalid: !lastName.isValid}">
           <label for="lastname">Lastname</label>
-          <input type="text" id="lastname" v-model.trim="lastName">
+          <input 
+            type="text" 
+            id="lastname" 
+            v-model.trim="lastName.val" 
+            @blur="clearValidity('lastName')"
+          >
+          <p v-if="!lastName.isValid">Nerd, fill the Lastname field</p>
       </div>
-      <div class="form-control">
+      <div class="form-control" :class="{invalid: !description.isValid}">
           <label for="description">Description</label>
-          <textarea id="description" rows="5" v-model.trim="description"></textarea>
+          <textarea 
+            id="description" 
+            rows="5" 
+            v-model.trim="description.val" 
+            @blur="clearValidity('description')"
+          ></textarea>
+          <p v-if="!description.isValid">Nerd, fill the Description field</p>
       </div>
-      <div class="form-control">
+      <div class="form-control" :class="{invalid: !rate.isValid}">
           <label for="rate">Horly Rate</label>
-          <input type="number" id="rate" v-model.number="rate">
+          <input 
+            type="number" 
+            id="rate" 
+            v-model.number="rate.val" 
+            @blur="clearValidity('rate')"
+          >
+          <p v-if="!rate.isValid">Nerd, rate should be greater than zero</p>
       </div>
-      <div class="form-control">
+      <div class="form-control" :class="{invalid: !areas.isValid}">
           <h3>Skills</h3>
           <div>
-              <input type="checkbox" value="frontend" id="frontend" v-model="areas">
+              <input 
+                type="checkbox" 
+                value="frontend" 
+                id="frontend" 
+                v-model="areas.val" 
+                @blur="clearValidity('areas')"
+              >
               <label for="frontend">Frontend Development</label>
           </div>
           <div>
-              <input type="checkbox" value="backend" id="backend" v-model="areas">
+              <input 
+                type="checkbox" 
+                value="backend" 
+                id="backend" 
+                v-model="areas.val" 
+                @blur="clearValidity('areas')"
+              >
               <label for="backend">Backend Development</label>
           </div>
           <div>
-              <input type="checkbox" value="career" id="career" v-model="areas">
+              <input 
+                type="checkbox" 
+                value="career" 
+                id="career" 
+                v-model="areas.val" 
+                @blur="clearValidity('areas')"
+              >
               <label for="career">Career Advisor</label>
           </div>
+          <p v-if="!areas.isValid">Nerd, pick at least one skill</p>
       </div>
+      <p v-if="!formIsValid">Nerd, check the input fields again :)</p>
       <base-button>Register</base-button>
   </form>
 </template>
@@ -40,21 +84,69 @@ export default {
     emits: ['save-data'],
     data(){
         return{
-            firstName: '',
-            lastName: '',
-            description: '',
-            rate: null,
-            areas: []
+            firstName: {
+              val: '',
+              isValid: true
+            },
+            lastName: {
+              val: '',
+              isValid: true
+            },
+            description: {
+              val: '',
+              isValid: true
+            },
+            rate: {
+              val: null,
+              isValid: true
+            },
+            areas: {
+              val: [],
+              isValid: true
+            },
+            formIsValid: true
         }
     },
     methods: {
+        clearValidity(input){
+          this[input].isValid = true
+        },
+        validateForm(){
+          this.formIsValid = true
+          if(this.firstName.val === ''){
+            this.firstName.isValid = false
+            this.formIsValid = false
+          }
+          if(this.lastName.val === ''){
+            this.lastName.isValid = false
+            this.formIsValid = false
+          }
+          if(this.description.val === ''){
+            this.description.isValid = false
+            this.formIsValid = false
+          }
+          if(!this.rate.val || this.rate.val < 0){
+            this.rate.isValid = false
+            this.formIsValid = false
+          }
+          if(this.areas.val.length === 0){
+            this.areas.isValid = false
+            this.formIsValid = false
+          }
+        },
         submitForm(){
+            this.validateForm()
+
+            if(!this.formIsValid) {
+              return
+            }
+
             const formData = {
-                first: this.firstName,
-                last: this.lastName,
-                desc: this.description,
-                rate: this.rate,
-                areas: this.areas
+                first: this.firstName.val,
+                last: this.lastName.val,
+                desc: this.description.val,
+                rate: this.rate.val,
+                areas: this.areas.val
             }
 
             this.$emit('save-data', formData)
