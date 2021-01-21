7aa21d0bf4bff85085598f177274e37955981d60
hendurhance <50846992+hendurhance@users.noreply.github.com>
2021-01-21 19:33:42 +0100

    NotFound page done with HandleError warn bug

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+    <section>
+        <base-card>
+            <h2>Sorry, page not found</h2>
+            <p>
+                Check out other pages Nerd :) , Do you mind checking our tutors
+                <router-link to="/tutors">Tutors</router-link>
+            </p>
+        </base-card>
+    </section>
+</template>
